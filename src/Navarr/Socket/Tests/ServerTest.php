29fad6315f3bd4cbf6c549113a28c5ec50c4b9d2
Navarr Barnier <me@navarr.me>
2013-03-23 03:11:51 -0400

    Better Testing & Travis CI Experimentation

--- src/Navarr/Socket/Tests/ServerTest.php
+++ src/Navarr/Socket/Tests/ServerTest.php
@@ -6,19 +6,32 @@ use Navarr\Socket\Server;
 
 class ServerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testServer()
+    public function testAddingHookWorksProperly()
     {
-        $this->markTestIncomplete("Can't Yet Test Servers");
-
-        $server = new Server('127.0.0.1', 9002);
+        $server = new Server('127.0.0.1', 9000);
+        $exampleReturn = '__NAVARR_SOCKET_TEST_EXAMPLE_RETURN__';
         $server->addHook(
             Server::HOOK_CONNECT,
-            function ($server, $client, $input) {
-                $this->assertTrue($server instanceof Server);
-                $this->assertTrue($client instanceof Socket);
-                return Server::RETURN_HALT_SERVER;
+            function () {
+                return '__NAVARR_SOCKET_TEST_EXAMPLE_RETURN__';
             }
         );
-        $server->run();
+
+        $serverClass = new \ReflectionClass($server);
+        $hooksProperty = $serverClass->getProperty('hooks');
+        $hooksProperty->setAccessible(true);
+
+        $hooks = $hooksProperty->getValue($server);
+
+        // Hooks should be a protected property
+        $this->assertTrue($hooksProperty->isProtected());
+        // Hooks should be an array
+        $this->assertTrue(is_array($hooks));
+        // Hooks should have a single callable in $hooks[Server::HOOK_CONNECT]
+        $this->assertEquals(1, count($hooks[Server::HOOK_CONNECT]));
+        // Values in $hooks[Server::HOOK_CONNECT] should be callables
+        $this->assertTrue(is_callable($hooks[Server::HOOK_CONNECT][0]));
+        // Make sure the callable in $hooks[Server::HOOK_CONNECT] is the return value we expect
+        $this->assertEquals($exampleReturn, call_user_func($hooks[Server::HOOK_CONNECT][0]));
     }
 }
