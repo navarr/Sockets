9851be42310b392ab59447fcffe6af355d0b2cae
Navarr Barnier <me@navarr.me>
2013-03-22 22:28:13 -0400

    Moving some things around

---
+++ autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ .'/vendor/autoload.php';
+
+function autoload($className)
+{
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strripos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require 'src\\' . $fileName;
+}
+
+spl_autoload_register('autoload');
