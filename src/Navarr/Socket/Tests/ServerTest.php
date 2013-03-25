2da8facb8b4f0567dd8861e0bf7955dac8350a84
Navarr Barnier <me@navarr.me>
2013-03-25 03:11:52 -0400

    Completed Code Coverage for Server.addHook

--- src/Navarr/Socket/Tests/ServerTest.php
+++ src/Navarr/Socket/Tests/ServerTest.php
@@ -33,17 +33,15 @@ class ServerTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(is_callable($hooks[Server::HOOK_CONNECT][0]));
         // Make sure the callable in $hooks[Server::HOOK_CONNECT] is the return value we expect
         $this->assertEquals($exampleReturn, call_user_func($hooks[Server::HOOK_CONNECT][0]));
+
+        return $server;
     }
 
-    public function testAddingMultipleHooksWorksProperly()
+    /**
+     * @depends testAddingSingleHookWorksProperly
+     */
+    public function testAddingMultipleHooksWorksProperly($server)
     {
-        $server = new Server('127.0.0.1', 9000);
-        $server->addHook(
-            Server::HOOK_CONNECT,
-            function () {
-                return '__NAVARR_SOCKET_TEST_EXAMPLE_RETURN_1__';
-            }
-        );
         $server->addHook(
             Server::HOOK_CONNECT,
             function () {
@@ -65,5 +63,34 @@ class ServerTest extends \PHPUnit_Framework_TestCase
         }
         // Make sure the callable in $hooks[Server::HOOK_CONNECT] is the return value we expect
         $this->assertEquals('__NAVARR_SOCKET_TEST_EXAMPLE_RETURN_2__', call_user_func($hooks[Server::HOOK_CONNECT][1]));
+
+        return $server;
+    }
+
+    /**
+     * @depends testAddingMultipleHooksWorksProperly
+     */
+    public function testAddingSameHookMultipleTimesWorksProperly($server)
+    {
+        $callable = function () {
+            return '__NAVARR_SOCKET_TEST_EXAMPLE_RETURN_3__';
+        };
+        $server->addHook(Server::HOOK_CONNECT, $callable);
+        $server->addHook(Server::HOOK_CONNECT, $callable);
+
+        $serverClass = new \ReflectionClass($server);
+        $hooksProperty = $serverClass->getProperty('hooks');
+        $hooksProperty->setAccessible(true);
+
+        $hooks = $hooksProperty->getValue($server);
+
+        // Hooks should have two callables in $hooks[Server::HOOK_CONNECT]
+        $this->assertEquals(3, count($hooks[Server::HOOK_CONNECT]));
+        // Values in $hooks[Server::HOOK_CONNECT] should be callables
+        foreach ($hooks[Server::HOOK_CONNECT] as $callable) {
+            $this->assertTrue(is_callable($callable));
+        }
+        // Make sure the callable in $hooks[Server::HOOK_CONNECT] is the return value we expect
+        $this->assertEquals('__NAVARR_SOCKET_TEST_EXAMPLE_RETURN_3__', call_user_func($hooks[Server::HOOK_CONNECT][2]));
     }
 }
